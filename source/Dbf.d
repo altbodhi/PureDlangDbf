7e592127b9a3ec7e71adbc86de0288a96548dcd2
noname <blevaka@gmail.com>
2017-11-09 22:09:30 +0700

    почти работает чтение сырых данных

--- source/Dbf.d
+++ source/Dbf.d
@@ -2,73 +2,150 @@ import std.stdio;
 import national.charsets;
 import national.encoding;
 import std.array;
-import std.file;	
+import std.file;
 import std.string;
 import std.datetime;
 import std.bitmanip;
+import std.conv;
 
 class DbfReader
 {
-File db;
+    File db;
+    FieldInfo[] fieldInfo;
+    dstring[dstring][] rows;
+    OneByteCodePage enc;
+    /**
+    * Windows1251 by default or IBM866(for example, see charsets.d)
+    **/
+    this(OneByteCodePage _enc = Windows1251)
+    {
+        enc = _enc;
+    }
 
-void openDbf(string path)
-{
-    db = File(path, "r");
+    void openDbf(string path)
+    {
+        db = File(path, "r");
+    }
+
+    @property int versionDbf()
+    {
+        db.seek(0);
+        return oneByte();
+    }
+
+    @property DateTime lastUpdate()
+    {
+        db.seek(1);
+        auto year = oneByte();
+        db.seek(2);
+        auto mm = oneByte();
+        db.seek(3);
+        auto dd = oneByte();
+        return DateTime(year < 70 ? year + 2000 : year + 1900, mm, dd);
+    }
+
+    private uint _recordCount = -1;
+    @property uint recordCount()
+    {
+        if (_recordCount < 0)
+        {
+            db.seek(4);
+            _recordCount = Int32();
+        }
+        return _recordCount;
+    }
+
+    private int _headSize = -1;
+    @property int headSize()
+    {
+        if (_headSize < 0)
+        {
+            db.seek(8);
+            _headSize = Int16();
+        }
+        return _headSize;
+    }
+
+    private int _recordSize = -1;
+    @property int recordSize()
+    {
+        if (_recordSize < 0)
+        {
+            db.seek(10);
+            _recordSize = Int16();
+        }
+        return _recordSize;
+    }
+
+    @property int fieldCount()
+    {
+        import std.math;
+
+        return cast(int) round(headSize / 33);
+    }
+
+    void readFieldInfo()
+    {
+
+        for (int i = 1; i <= fieldCount; i++)
+        {
+            db.seek(i * 32);
+            ubyte[] b;
+            b.length = 33;
+            auto a = db.rawRead(b);
+            fieldInfo ~= FieldInfo(enc.decode(a[0 .. 10]).array, a[11], cast(int) a[16]);
+        }
+    }
+
+    void loadRows(int max = 0)
+    {
+        for (int i = 0; i < (max > 0 ? max : recordCount); i++)
+        {
+            db.seek(32 + headSize + i * recordSize);
+            ubyte[] b;
+            b.length = recordSize;
+            auto a = db.rawRead(b);
+            dstring[dstring] row;
+            row["[*]"] = enc.decode(a[0 .. 0]).array;
+            int point = 1;
+            foreach (fi; fieldInfo)
+            {
+                row[fi.name] = enc.decode(a[point .. point + fi.len]).array;
+                point += fi.len;
+            }
+            rows ~= row;
+        }
+    }
+
+    uint Int32()
+    {
+        ubyte[] buffer;
+        buffer.length = 4;
+        auto a = db.rawRead(buffer);
+        return peek!(uint, Endian.littleEndian)(a);
+    }
+
+    ushort Int16()
+    {
+        ubyte[] buffer;
+        buffer.length = 2;
+        auto a = db.rawRead(buffer);
+        return peek!(ushort, Endian.littleEndian)(a);
+    }
+
+    uint oneByte()
+    {
+        byte[] buffer;
+        buffer.length = 1;
+        auto data = db.rawRead(buffer);
+        return cast(uint) data[0];
+    }
+
 }
 
-int versionDbf()
+struct FieldInfo
 {
-    db.seek(0);
-    return oneByte();
+    dstring name;
+    char type;
+    int len;
 }
-DateTime lastUpdate()
-{
-    db.seek(1);
-    auto year = oneByte();
-    db.seek(2);
-    auto mm = oneByte();
-    db.seek(3);
-    auto dd = oneByte();
-    return DateTime(year<70 ? year+2000 : year + 1900,mm,dd);
-}
-
-uint  recordCount()
-{
-    db.seek(4); 
-    return Int32();
- }
- int headSize()
- {
-     db.seek(8); 
-    return Int16();
- }
- int recordSize()
- {
-     db.seek(10);
-     return Int16();
- }
- uint Int32()
- {
-     ubyte[] buffer;
-     buffer.length = 4;
-     auto a = db.rawRead(buffer);
-     return  peek!(uint, Endian.littleEndian)(a);
- }
-  ushort Int16()
- {
-     ubyte[] buffer;
-     buffer.length = 2;
-     auto a = db.rawRead(buffer);
-     return   peek!(ushort, Endian.littleEndian)(a);
- }
- uint oneByte()
- {
-    byte[] buffer;
-    buffer.length = 1;
-    auto data = db.rawRead(buffer);
-    return cast(uint)data[0];
- }
-}
-
-
-	 
--- source/app.d
+++ source/app.d
@@ -1,20 +1,34 @@
 import std.stdio;
 import Dbf;
+import national.charsets;
+import national.encoding;
 
 void main(string[] args)
 {
-   
-	/*dstring s = "Жил-были дед да баба.";
-	auto cp1251 = Windows1251.encode(s).array;
-	write("1251.txt",cp1251);*/
-	//dauto db = new DbfFile();
-	//new DbfColumn("fio", DbfColumnType.Character, 100, 0);
-	//write(col);
-	 auto reader = new DbfReader();
-     reader.openDbf("PIndx16.dbf");
-	 writeln("version File = ",reader.versionDbf());
-	 writeln("lastUpdate = ",reader.lastUpdate());
-	 writeln("recordCount = ",reader.recordCount());
-	 writeln("headSize = ",reader.headSize());
-	  writeln("recordSize = ",reader.recordSize());
+	auto reader = new DbfReader(IBM866);
+	reader.openDbf("PIndx16.dbf");
+	writeln("version File = ", reader.versionDbf);
+	writeln("lastUpdate = ", reader.lastUpdate);
+	writeln("recordCount = ", reader.recordCount);
+	writeln("headSize = ", reader.headSize);
+	writeln("recordSize = ", reader.recordSize);
+	writeln("fieldCount = ", reader.fieldCount);
+	reader.readFieldInfo();
+	int recSize = 0;
+	foreach (i, fi; reader.fieldInfo)
+	{
+		writeln("[", i, "] name = ", fi.name, "; type = ", fi.type, "; len = ", fi.len);
+		recSize += fi.len;
+	}
+	writeln("sum len = ", recSize);
+	reader.loadRows(1);
+	foreach (row; reader.rows)
+	{
+		foreach (key; row.keys)
+		{
+			writefln("%s = %s", key, row[key]);
+		}
+	}
+	writeln("loaded rows = ", reader.rows.length);
+
 }
